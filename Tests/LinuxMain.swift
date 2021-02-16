ca0fb0a5271c830567065168774d75e1113cc3f1
ipmanuel <manuel_pauls@gmx.de>
2021-02-16 15:46:45 +0100

    Updated LinuxMain.swift to run all the tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,5 +2,13 @@ import XCTest
 @testable import XMLDatabaseTests
 
 XCTMain([
-    testCase(XMLDatabaseTests.allTests),
+    testCase(XMLObjectTests.allTests),
+    testCase(FileDataManagerTests.allTests),
+    testCase(XMLInfoObjectTests.allTests),
+    testCase(XMLDocumentContainerTests.allTests),
+    testCase(XMLDocumentManagerTests.allTests),
+    testCase(XMLObjectsManagerTests.allTests),
+    testCase(XMlObjectMapperTests.allTests),
+    testCase(PersonMapperTests.allTests),
+    testCase(PersonTests.allTests)
 ])
